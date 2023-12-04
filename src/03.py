8cc972dea11881de624c0c370d374ed8667526dd
Nicholas Sherrill <nsherrill@messerfinancial.com>
2023-12-04 11:25:53 -0500

    day 3

--- src/03.py
+++ src/03.py
@@ -1,4 +1,5 @@
 from utils.api import get_input, get_test_input
+import time
 
 current_day = 3
 input_str = get_input(current_day)
@@ -45,10 +46,64 @@ def part1(inp):
     return sum(okNums)
 
 
-part1TestExpected = 413
+def inBubble(numLoc, bubble):
+    for loc in numLoc[1]:
+        if loc in bubble:
+            return True
+    return False
+
+
+def part2(inp):
+    lines = inp.splitlines()
+    numberLocs = []
+    gearLocs = []
+    rashos = []
+    for lineNum in range(0, len(lines)):
+        line = lines[lineNum]
+        currentNum = ''
+        currentNumLocs = []
+        for colNum in range(0, len(line)):
+            myChr = lines[lineNum][colNum]
+            if myChr.isnumeric():
+                currentNum += myChr
+                currentNumLocs.append((lineNum, colNum))
+            elif currentNum != '':
+                numberLocs.append((int(currentNum), currentNumLocs))
+                currentNum = ''
+                currentNumLocs = []
+            if myChr == '*':
+                gearLocs.append((lineNum, colNum))
+        if currentNum != '':
+            numberLocs.append((int(currentNum), currentNumLocs))
+    for loc in gearLocs:
+        bubble = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                bubble.append((loc[0] + i, loc[1] + j))
+        nearGear = list(filter(lambda x: inBubble(x, bubble), numberLocs))
+        if len(nearGear) == 2:
+            s = 1
+            for g in nearGear:
+                s = s * g[0]
+            rashos.append(s)
+    return sum(rashos)
+
+
+startTime = time.time()
+
+part1TestExpected = 4361
 part1Test = part1(test_str)
 if part1Test != part1TestExpected:
     raise Exception("TEST FAILED! Expected {exp}, Got {rel}".format(exp=part1TestExpected, rel=part1Test))
 part1Real = part1(input_str)
 
+part2TestExpected = 467835
+part2Test = part2(test_str)
+if part2Test != part2TestExpected:
+    raise Exception("TEST FAILED! Expected {exp}, Got {rel}".format(exp=part2TestExpected, rel=part2Test))
+part2Real = part2(input_str)
+
 print(part1Real)
+print(part2Real)
+
+print("--- %s seconds ---" % (time.time() - startTime))
