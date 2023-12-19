1f4e3519b606de6d4bb735c14a997954c04fc90e
Nicholas Sherrill <nsherrill@messerfinancial.com>
2023-12-19 15:18:05 -0500

    don't include inputs in git repo

--- src/all.py
+++ src/all.py
@@ -4,10 +4,8 @@ import time
 
 if __name__ == '__main__':
     os.chdir('../')
-    directory = 'inputs'
     startTime = time.time()
-    for filename in os.listdir(directory):
-        pyFileName = 'src/' + filename + '.py'
-        print(pyFileName)
-        runpy.run_path(pyFileName)
+    for filename in os.listdir('./src'):
+        if filename.split('.')[0].isnumeric():
+            runpy.run_path('src/'+filename)
     print("\033[96mTime to execute all: %s seconds\033[0m" % (time.time() - startTime))
