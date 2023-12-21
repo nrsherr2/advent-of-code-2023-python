0d7197b15da318e340705dc5170e4ebc2ab6d57f
Nicholas Sherrill <nsherrill@messerfinancial.com>
2023-12-21 17:29:05 -0500

    day 21 part 2 first attempt

--- src/21.py
+++ src/21.py
@@ -8,32 +8,126 @@ test_str = get_test_input(current_day)
 
 def part1(fullInput, numSteps):
     staringLoc, rocLocs = parseInput(fullInput)
-    # start on 1 for even numbers, 0 for odd
     currentLocs = set()
     currentLocs.add(staringLoc)
+    numLines = len(fullInput.splitlines())
+    numCols = len(fullInput.splitlines()[0])
+    return len(calcLocs(numSteps, numLines, numCols, currentLocs, rocLocs))
+
+
+def calcLocs(numSteps, numLines, numCols, currentLocs, rocLocs):
     for i in range(numSteps):
         newLocs = set()
         for loc in currentLocs:
             for d in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 nl = (loc[0] + d[0], loc[1] + d[1])
-                if nl not in rocLocs:
+                if nl not in rocLocs and 0 <= nl[0] < numLines and 0 <= nl[1] < numCols:
                     newLocs.add(nl)
         currentLocs = newLocs
-    return len(currentLocs)
+    return currentLocs
 
 
 def part2(fullInput):
-    print(fullInput)
+    # calculate the number of spaces if we can freely move an even number of steps
+    numEvenInFull = part1(fullInput, 130)
+    # calculate the number of spaces if we can free move an odd number of steps
+    numOddInFull = part1(fullInput, 131)
+    numLines = len(fullInput.splitlines())
+    numCols = len(fullInput.splitlines()[0])
+    _, rocLocs = parseInput(fullInput)
+    lPoints = calcLocs(131, numLines, numCols, {(65, 0)}, rocLocs)
+    lScore = len(lPoints)
+
+    rPoints = calcLocs(131, numLines, numCols, {(65, 130)}, rocLocs)
+    rScore = len(rPoints)
+
+    bPoints = calcLocs(131, numLines, numCols, {(130, 65)}, rocLocs)
+    bScore = len(bPoints)
+
+    tPoints = calcLocs(131, numLines, numCols, {(0, 65)}, rocLocs)
+    tScore = len(tPoints)
+
+    lbPoints = lPoints.union(bPoints)
+    lbScore = len(lbPoints)
+
+    ltPoints = lPoints.union(tPoints)
+    ltScore = len(ltPoints)
+
+    rtPoints = rPoints.union(tPoints)
+    rtScore = len(rtPoints)
+
+    rbPoints = rPoints.union(bPoints)
+    rbScore = len(rbPoints)
+
+    numStepsLeft = 26501365
+    totalCurrentPoints = numOddInFull
+    totalCurrentPoints += parallelUniverseVert(numEvenInFull, numOddInFull, tScore, numStepsLeft - 65, True)
+    totalCurrentPoints += parallelUniverseVert(numEvenInFull, numOddInFull, bScore, numStepsLeft - 65, True)
+    totalCurrentPoints += parallelUniverseHoriz(numEvenInFull, numOddInFull, rScore, rtScore, rbScore,
+                                                numStepsLeft - 65, True)
+    totalCurrentPoints += parallelUniverseHoriz(numEvenInFull, numOddInFull, lScore, ltScore, lbScore,
+                                                numStepsLeft - 65, True)
+    # totalCurrentPoints = (numOddInFull
+    #                       + parallelUniverseVert(numEvenInFull, numOddInFull, tScore, numStepsLeft - 65, True)
+    #                       + parallelUniverseVert(numEvenInFull, numOddInFull, bScore, numStepsLeft - 65, True)
+    #                       + parallelUniverseHoriz(numEvenInFull, numOddInFull, rScore, rtScore, rbScore,
+    #                                               numStepsLeft - 65, True)
+    #                       + parallelUniverseHoriz(numEvenInFull, numOddInFull, lScore, ltScore, lbScore,
+    #                                               numStepsLeft - 65, True))
+    return totalCurrentPoints
 
 
-def genDiamond(step, startingLoc):
-    points = set()
-    for i in range(-1 * step, step + 1):
-        j = step - abs(i)
-        j1 = -1 * j
-        points.add((startingLoc[0] + i, startingLoc[1] + j))
-        points.add((startingLoc[0] + i, startingLoc[1] + j1))
-    return points
+def parallelUniverseHoriz(numEvenInFull, numOddInFull, hScore, htScore, hbScore, stepsLeft, evenStep):
+    s = stepsLeft
+    sc = 0
+    es = evenStep
+    print(s)
+    while s > 131:
+        if str(s)[-2:] == '00':
+            print(s)
+        thisScore = numEvenInFull if evenStep else numOddInFull
+        uScore = parallelUniverseVert(numEvenInFull, numOddInFull, htScore, s - 131, not evenStep)
+        lScore = parallelUniverseVert(numEvenInFull, numOddInFull, hbScore, s - 131, not evenStep)
+        sc += (thisScore + uScore + lScore)
+        es = not es
+        s -= 131
+    return sc + hScore
+    # thisScore = numEvenInFull if evenStep else numOddInFull
+    # print(stepsLeft)
+    # if stepsLeft > 131:
+    #     return (thisScore
+    #             + parallelUniverseHoriz(numEvenInFull, numOddInFull, hScore, htScore, hbScore, stepsLeft - 131,
+    #                                     not evenStep)
+    #             + parallelUniverseVert(numEvenInFull, numOddInFull, htScore, stepsLeft - 131, not evenStep)
+    #             + parallelUniverseVert(numEvenInFull, numOddInFull, hbScore, stepsLeft - 131, not evenStep))
+    # else:
+    #     return hScore
+
+
+def parallelUniverseVert(numEvenInFull, numOddInFull, nScore, stepsLeft, evenStep):
+    s = stepsLeft
+    sc = 0
+    es = evenStep
+    while s > 131:
+        thisScore = numEvenInFull if evenStep else numOddInFull
+        sc += thisScore
+        es = not es
+        s -= 131
+    score1 = sc + nScore
+    # (numLeft1, numLeft2)= divmod(stepsLeft,131)
+    # if evenStep:
+    #     score2 = numLeft1 * numEvenInFull + numLeft2 * numOddInFull + nScore
+    # else:
+    #     score2 = numLeft1 * numOddInFull + numLeft2 * numEvenInFull + nScore
+    # if(score1 != score2):
+    #     raise Exception(score1,score2)
+    return score1
+    # thisScore = numEvenInFull if evenStep else numOddInFull
+    # print(stepsLeft)
+    # if stepsLeft > 131:
+    #     return thisScore + parallelUniverseVert(numEvenInFull, numOddInFull, nScore, stepsLeft - 131, not evenStep)
+    # else:
+    #     return nScore
 
 
 def parseInput(fullInput):
@@ -58,7 +152,7 @@ if part1Test != part1TestExpected:
     raise Exception(part1TestExpected, part1Test)
 print_hlight(part1(input_str, 64))
 
-part2Test = part2(test_str)
+# part2Test = part2(test_str)
 print_hlight(part2(input_str))
 
 print_tlight("--- %s seconds ---" % (time.time() - startTime))
